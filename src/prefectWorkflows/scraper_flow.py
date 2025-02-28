a85022cb8102dea1768d5b49c84e3e4e7c959977
Nikhil <nikhilkudupudi@gmail.com>
2025-02-28 08:41:41 -0500

    add prefect worflow for dag

---
+++ src/prefectWorkflows/scraper_flow.py
@@ -0,0 +1,30 @@
+from prefect import flow, task
+from src.data_preprocessing.scraper import  scrape_and_load_task 
+from src.data_preprocessing.preprocess_data import trainModel
+from prefect.task_runners import SequentialTaskRunner
+# Decorate the imported functions with @task
+from prefect import config
+
+config.prefect.runner.process_limit = 1
+
+
+@task
+def scrape_all_urls_task():
+    # If scrape_all_urls is an imported function, call it here and return the result
+    return scrape_and_load_task()  # or return the relevant data
+@task
+def trainModel_task():
+    return trainModel()
+
+@flow(task_runner=SequentialTaskRunner())
+def scraperflow():
+    # Use the tasks within the flow
+    scrape_all_urls_task()
+    trainModel_task(wait_for=[scrape_all_urls_task])
+
+if __name__ == "__main__":
+# Run the flow
+     scraperflow.serve(name="my-first-deployment",
+                      tags=["onboarding"],
+                      interval=60)
+
